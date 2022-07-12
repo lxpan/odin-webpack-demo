95c5ea6863dae2af72de94eef3e04d1651238a82
Luoxi Pan <l.panpax@gmail.com>
2022-07-12 16:28:28 +1000

    Create CSS file and add to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // use your function!
     element.textContent = myName('Luoxi');
+    element.classList.add('hello');
 
     // Lodash, now imported by this script
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
