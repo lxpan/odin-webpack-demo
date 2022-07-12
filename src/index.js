191f07acc8655d64526ec6025af3e6322a238281
Luoxi Pan <l.panpax@gmail.com>
2022-07-12 16:40:15 +1000

    Remove custom create module

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import _ from 'lodash';
-import myName from './myName';
 import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
-    // use your function!
-    element.textContent = myName('Luoxi');
-    element.classList.add('hello');
-
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
