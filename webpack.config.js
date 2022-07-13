f772e677248222300ab2ffb20e77b93a798a3b26
Luoxi Pan <l.panpax@gmail.com>
2022-07-13 14:27:09 +1000

    Enable output.clean

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,8 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'bundle.js',
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
